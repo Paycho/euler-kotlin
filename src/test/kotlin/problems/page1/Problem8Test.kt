20e3b471f71c8d8ec78010a8b0215ec81c5f8507
Matt <Paycho@users.noreply.github.com>
2020-09-10 18:41:45 -0600

    Problem 8

---
+++ src/test/kotlin/problems/page1/Problem8Test.kt
@@ -0,0 +1,12 @@
+package problems.page1
+
+import org.junit.Assert
+import org.junit.Test
+
+class Problem8Test {
+
+  @Test
+  fun problem8Test() {
+    Assert.assertEquals(5832, Problem8.greatestProductQueue(4))
+  }
+}
