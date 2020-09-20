f321c2301ffd5e3d4609e922039b59c89f744be0
Matt <Paycho@users.noreply.github.com>
2020-09-20 14:17:34 -0600

    Change type of problemMap to <Int, Problem> so that things sort themselves naturally better

--- src/main/kotlin/ProblemIndex.kt
+++ src/main/kotlin/ProblemIndex.kt
@@ -3,7 +3,7 @@ import base.Problem
 import org.reflections.Reflections
 
 object ProblemIndex {
-  private val problemMap = HashMap<String, Problem>()
+  private val problemMap = HashMap<Int, Problem>()
 
   fun generateProblemMap() {
     val reflections = Reflections("problems")
@@ -11,7 +11,7 @@ object ProblemIndex {
     for (entry in problemSet) {
       val problem = entry.kotlin.objectInstance
       if (problem != null) {
-        problemMap[problem.getNumber()] = problem
+        problemMap[problem.getNumber().toInt()] = problem
       }
     }
   }
@@ -22,11 +22,11 @@ object ProblemIndex {
 
   fun solveProblem(number: String) {
     println("Solving problem $number:")
-    println(problemMap[number]?.solve() ?: "Invalid Problem Number")
+    println(problemMap[number.toInt()]?.solve() ?: "Invalid Problem Number")
   }
 
   fun describe(number: String) {
     println("Describing problem $number:")
-    println(problemMap[number]?.getDescription()?.trimIndent() ?: "Invalid Problem Number")
+    println(problemMap[number.toInt()]?.getDescription()?.trimIndent() ?: "Invalid Problem Number")
   }
 }
